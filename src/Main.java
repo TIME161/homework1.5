f6ce3edcdb1e5b3f8209c64ded4fc0e955c44991
TIME <opout777@yandex.ru>
2023-01-17 21:40:14 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -16,4 +17,26 @@ public class Main {
             System.out.println("Ваша система не распознана");
         }
     }
-}
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int number = 1;
+        int clientDeviceYear = 2014;
+        if (number == 0 || number == 1) {
+            if (number == 0) {
+                if (2015 > clientDeviceYear) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+                else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");}
+            }
+            if (number == 1) {
+                if (2015 > clientDeviceYear) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");}
+                else {
+                    System.out.println("Установите версию приложения для Android по ссылке");}
+            }
+        }
+        else {
+                System.out.println("Ваша система не распознана");}
+        }
+    }
