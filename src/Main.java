5bea7da11be6108941b420e1066e608052936512
TIME <opout777@yandex.ru>
2023-01-17 16:33:00 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int number = 5;
+        if (number == 0 || number == 1) {
+            if (number == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Ваша система не распознана");
+        }
     }
 }
